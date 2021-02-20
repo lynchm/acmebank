7f7aeb8526f9b46d510b0ff56d0db409ca79f8ef
lynchm <lmachabango@gmail.com>
2021-02-20 22:20:24 +0200

    validation on maximum overdraft on current account

--- webapp/controller/Main.controller.js
+++ webapp/controller/Main.controller.js
@@ -15,6 +15,7 @@ sap.ui.define([
 		var sServiceUrl = "http://localhost:8080/api/accounts";
 		return Controller.extend("acmebank.controller.Main", {
 			onInit: function () {
+				sap.ui.core.BusyIndicator.show(0);
 				var oModel = new sap.ui.model.json.JSONModel();
 				var that = this;
 				$.ajax({
@@ -23,20 +24,42 @@ sap.ui.define([
 					async: false,
 			  success: function(data, textStatus, jqXHR) {
 					var balances = [];
-					console.log(data); 
+					console.log(that.setWithdrawalLimit(data));
+					console.log(data);
 					oModel.setData(data);
 					that.getView().setModel(oModel);
 					data.forEach(function(account_item) {
 					   balances.push(account_item.balance);
 					});
 					that.calculateBalance(balances);
+					
+					
+					sap.ui.core.BusyIndicator.hide();
 			     },fail: function(jqXHR, textStatus, error){
 					console.log(jqXHR.responseText);
+					sap.ui.core.BusyIndicator.hide();
 				 }
 				})
 		
 			},
-			
+			//enabling or 
+			setWithdrawalLimit: function(account_list) {
+				account_list.forEach(function(account, index) 
+				 {
+					if(account.account_type=="savings" && parseFloat(account.balance) < 0)
+					{
+						account_list[index].limit_reached = false;
+
+					}else if(account.account_type=="cheque" && parseFloat(account.balance) <= parseFloat(-500))
+					{
+						account_list[index].limit_reached = false;
+					}else
+					{
+						account_list[index].limit_reached = true;
+					}
+				});
+				return account_list;
+			},
 			//function for calculating total account balance 
 			calculateBalance: function(params) {
 				var that = this;
